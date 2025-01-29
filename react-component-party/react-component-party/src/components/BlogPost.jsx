e8fbe45d2ab4fc6a51f348ea94096724416d4753
Jaume Esquerdo <jaume.esquerdo@alumnos.cei.es>
2025-01-29 16:04:07 +0100

    format

--- react-component-party/react-component-party/src/components/BlogPost.jsx
+++ react-component-party/react-component-party/src/components/BlogPost.jsx
@@ -1,11 +1,11 @@
-const BlogPost = ({titulo, fecha, autor, children}) => {
-    return ( 
+const BlogPost = ({ titulo, fecha, autor, children }) => {
+    return (
         <div>
             <h2>{titulo}</h2>
             <p>Publicado el {fecha} por {autor}.</p>
             <div>{children}</div>
         </div>
-     );
+    );
 }
 
 export default BlogPost;
