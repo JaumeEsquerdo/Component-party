9da6a61071ebc35bd034d9e09154181b889c6647
Jaume Esquerdo <jaume.esquerdo@alumnos.cei.es>
2025-01-29 20:25:26 +0100

    feat actividad 10

--- react-component-party/react-component-party/src/components/AlertBox.jsx
+++ react-component-party/react-component-party/src/components/AlertBox.jsx
@@ -2,7 +2,8 @@ const AlertBox = ({ status, children }) => {
 
     
     return (
-        <div>
+        <div className={`AlertBox AlertBox--${status}`}>
+            {/* el status de arriba se pone con $, ya que es una parte de HTML */}
 
             {children}
         </div>
