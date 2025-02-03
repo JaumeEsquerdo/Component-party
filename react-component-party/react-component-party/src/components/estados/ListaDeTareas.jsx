cf9b35f5a2f41e3f1da7695286df8b7b64d0c451
Jaume Esquerdo <jaume.esquerdo@alumnos.cei.es>
2025-02-03 23:58:40 +0100

    feat dia 8

--- react-component-party/react-component-party/src/components/estados/ListaDeTareas.jsx
+++ react-component-party/react-component-party/src/components/estados/ListaDeTareas.jsx
@@ -9,27 +9,41 @@ const ListaDeTareas = () => {
 
 
         if (tarea.trim() !== "") {
-            setTareas([...tareas, tarea])
+            setTareas([...tareas, { texto: tarea, checked: false }])
             //El ...tareas extrae los elementos del array tareas y los coloca dentro del nuevo array.
             //Luego, agrega tarea como el último elemento.
             //Si pones [tareas, tarea] se generan una lista con otra lista (la de tareas) dentro, y por fuera la nueva tarea
-             setTarea(""); //volver a poner tarea a 0, cuando he agregado la tarea
+            setTarea(""); //volver a poner tarea a 0, cuando he agregado la tarea
             console.log(tareas)
 
         } else {
             alert("tarea no correcta");
         }
-
     }
+
+    //marcar o desmaarcar tarea
+    const handleCheck = (index) => {
+        const nuevasTareas = tareas.map((t, i) => {
+            return i == index ? { ...t, checked: !t.checked }
+                :
+                t
+        })
+        setTareas(nuevasTareas);
+    }
+
+    const handleEliminarTarea = () => {
+        setTareas(tareas.filter((t) => !t.checked));
+    };
 
 
 
     return (
         <>
             <h3>Voy a agregar la tarea "{tarea}"</h3>
-            
+
             {/* e.target.value obtiene el valor actual del input */}
             {/* setTarea(e.target.value) actualiza el estado tarea con el nuevo valor */}
+            {/* e.target.value es la propiedad del elemento <input> que contiene el texto actual que el usuario ha escrito. */}
             <input type="text" value={tarea} onChange={(e) => setTarea(e.target.value)} placeholder="Escribe una nueva tarea" />
             <button onClick={handleAgregarTarea}>Agregar tarea</button>
 
@@ -38,11 +52,20 @@ const ListaDeTareas = () => {
                 {
                     tareas.map((t, i) =>
 
-                        (<li key={i}>{t}</li>)
+                    (<li key={i}>
+                        <label >
+                        <input type="checkbox"
+                            checked={t.checked}
+                            onChange={() => handleCheck(i)}
+                            // onChange es un evento en React que se dispara cada vez que cambia el valor de un elemento de entrada.
+
+
+                        />
+                        {t.texto}
+                        </label>
+                    </li>)
                     )
                 }
-
-
                 {/* {
                 tareas.map( (t,i)=>{
 
@@ -50,6 +73,9 @@ const ListaDeTareas = () => {
                 })
             } */}
             </ul>
+            <button onClick={handleEliminarTarea}>Eliminar tarea marcada</button>
+
+
         </>
     );
 
