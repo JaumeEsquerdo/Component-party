32a14fb47a740c3b6fe282a1d8459fc901d8fe19
Jaume Esquerdo <jaume.esquerdo@alumnos.cei.es>
2025-02-13 14:12:40 +0100

    icono que sube y baja

--- react-component-party/react-component-party/src/menus/MenuCategorias.jsx
+++ react-component-party/react-component-party/src/menus/MenuCategorias.jsx
@@ -9,10 +9,22 @@ export const MenuCategorias = ({ listaItems }) => {
 
     const handleOpen = (index) => {
         setMenuOpen((prev)=> ({
-            ...prev, [index]: !menuOpen[index],
+            ...prev, [index]: !prev[index],
+
+            // [index] → Define la clave del objeto que queremos cambiar.
+            // !prev[index] → Obtiene el valor actual en esa clave y lo invierte.
+
+
             //alterna el estado del menuOpen : const menuOpen = {0: false, 1: true, 2: false};
             //recordatorio: para entrar a false de 0 seria [0]: menuOpen[0] se puede llegar al false tanto con . como con[] la ser un objeto
             //para el arreglo se usa el indice directamente
+
+
+            // setMenuOpen((prev) => ({
+            //     ...{ 0: true, 1: false, 2: true }, // Copia todo el estado
+            //     [1]: !prev[1]  // Cambia la clave "1" → false se convierte en true
+            // }));
+
         }))
 
     };
@@ -33,11 +45,16 @@ export const MenuCategorias = ({ listaItems }) => {
 }
 
 const Item = ({ name, icon, items, index, handleOpen, menuOpen }) => {
-    console.log(name, icon, items)
+    console.log(name, icon, items);
     return (
         <li className='Menu-li'>
             <div onClick={()=> handleOpen(index)} className='Menu-title'>
                 <i className={`fa-solid ${icon}`}></i>{name}
+                <i
+                className={`fa-solid fa-house ${menuOpen[index]? "open": "closed"}`}>
+
+                </i>
+                {/* {menuOpen[index]?"x":"S"} */}
             </div>
 
             { menuOpen[index] && items.map(({ label, url }, i) => (
