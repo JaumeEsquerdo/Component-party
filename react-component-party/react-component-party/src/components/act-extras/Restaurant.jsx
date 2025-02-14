5c9ba6a6c1ce7e8d02b9c51733daea432eee2915
Jaume Esquerdo <jaume.esquerdo@alumnos.cei.es>
2025-02-14 19:40:45 +0100

    actualizacion antes de bug

--- react-component-party/react-component-party/src/components/act-extras/Restaurant.jsx
+++ react-component-party/react-component-party/src/components/act-extras/Restaurant.jsx
@@ -4,6 +4,7 @@ export const ListaDeCategorias = ({restaurant}) => {
     const {entrantes, principales, postres} = restaurant.menu
     return ( <div style={{background:"lightgreen"}}>
         <h1 >Menu</h1>
+        {entrantes.map(())}
         {postres.map(({id, nombre, precio, descripcion},i)=>(
             <p key={i}>
                 {nombre}...{precio} €
