01472cb3a92fc7e97abaa7e831932d8c64c2b5cd
Jaume Esquerdo <jaume.esquerdo@alumnos.cei.es>
2025-02-14 20:38:17 +0100

    menu

--- react-component-party/react-component-party/src/components/act-extras/Restaurant.jsx
+++ react-component-party/react-component-party/src/components/act-extras/Restaurant.jsx
@@ -1,18 +1,65 @@
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 
-export const ListaDeCategorias = ({restaurant}) => {
-    const {entrantes, principales, postres} = restaurant.menu
-    return ( <div style={{background:"lightgreen"}}>
+export const ListaDeCategorias = ({ restaurant }) => {
+    const { entrantes, principales, postres } = restaurant.menu
+    return (<div style={{ background: "lightgreen" }}>
         <h1 >Menu</h1>
-        {entrantes.map(())}
-        {postres.map(({id, nombre, precio, descripcion},i)=>(
+        <ListaPlatos menu={{ entrantes, principales, postres }} />
+        {/* {entrantes.map(())} */}
+        {postres.map(({ id, nombre, precio, descripcion }, i) => (
             <p key={i}>
                 {nombre}...{precio} €
             </p>
         ))}
-        
 
 
-    </div> );
+
+    </div>);
+
+
 }
- 
+
+const ListaPlatos = ({ menu }) => {
+    const { entrantes, principales, postres } = menu;
+    return (<div>
+        <h2>Entrantes</h2>
+
+        {entrantes.map((entrante) => (
+            <div key={entrante.id}>
+                <Plato plato={entrante} />
+            </div>
+
+        ))}
+        <h2>Principales</h2>
+
+        {principales.map((principal) => (
+            <div key={principal.id}>
+                <Plato plato={principal} />
+            </div>
+        ))}
+
+        <h2>Postres</h2>
+
+        {postres.map((postre) => (
+            <div key={postre.id}>
+                <Plato plato={postre} />
+            </div>
+        ))}
+
+    </div>
+    );
+}
+
+
+const Plato = ({ plato }) => {
+    const { nombre, precio, descripcion } = plato;
+    return (
+        <div>
+            <h3>{nombre}</h3>
+            <p>Precio {precio} €</p>
+            <p>{descripcion}</p>
+        </div>
+    );
+}
+
+export default Plato;
